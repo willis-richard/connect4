03b18059dc559de9510082c1724db0849d24a7e4
Muff2n <richard.willis5@gmail.com>
2018-11-21 12:40:08 +0000

    split files

--- src/main.py
+++ src/main.py
@@ -1,1 +1,9 @@
 """Hi"""
+
+from src import game
+
+if __name__ == "__main__":
+    name_o = str(input("Enter player o's name: "))
+    name_x = str(input("Enter player x's name: "))
+    game = game.Connect4(name_o, name_x, True, False)
+    game.play()
--- src/test/test_game.py
+++ src/test/test_game.py
@@ -1,4 +1,5 @@
-from src import game
+from src import board
+
 import pytest
 
 import numpy as np
@@ -31,11 +32,11 @@ pieces_1 = [
               [0, 0, 0, 0, 1, 1, 1],
               [1, 0, 0, 1, 0, 1, 0]], dtype=np.bool_),
     np.array([[0, 0, 0, 0, 0, 0, 0],
-              [0, 0, 0, 0, 0, 0, 0],
               [0, 0, 0, 1, 0, 0, 0],
-              [0, 0, 1, 0, 0, 0, 0],
-              [0, 1, 0, 0, 0, 0, 0],
-              [1, 1, 1, 0, 0, 0, 0]], dtype=np.bool_)
+              [0, 0, 0, 0, 0, 0, 0],
+              [0, 0, 0, 0, 1, 1, 0],
+              [0, 0, 0, 1, 0, 1, 0],
+              [1, 1, 1, 0, 0, 1, 0]], dtype=np.bool_)
 
 
     ]
@@ -64,14 +65,14 @@ pieces_2 = [
               [0, 0, 0, 0, 1, 1, 1],
               [0, 0, 0, 1, 0, 0, 0],
               [0, 0, 0, 0, 1, 0, 1]], dtype=np.bool_),
-    np.array([[0, 0, 0, 0, 0, 0, 0],
-              [0, 0, 0, 0, 0, 0, 0],
+    np.array([[0, 0, 0, 1, 0, 0, 0],
+              [0, 0, 0, 0, 1, 0, 0],
+              [0, 0, 0, 1, 1, 0, 0],
               [0, 0, 0, 1, 0, 0, 0],
-              [0, 0, 1, 0, 0, 0, 0],
-              [0, 1, 0, 0, 0, 0, 0],
-              [1, 1, 1, 0, 0, 0, 0]], dtype=np.bool_)
+              [0, 0, 0, 0, 1, 0, 0],
+              [0, 0, 0, 1, 1, 0, 0]], dtype=np.bool_)
     ]
-ans = [1, 1, 1, 2, 2]
+ans = [1, 1, 1, 2, None]
 
 assert len(pieces_1) == len(pieces_2) == len(ans)
 
@@ -79,7 +80,7 @@ assert len(pieces_1) == len(pieces_2) == len(ans)
                          [(n, p1, p2, a) for n, p1, p2, a in zip(range(len(ans)), pieces_1, pieces_2, ans)])
 def test_check_valid(n, pieces_1, pieces_2, ans):
     print("test_check_valid", n)
-    board = game.Board(white_pieces=pieces_1,
+    board = board.Board(white_pieces=pieces_1,
                        black_pieces=pieces_2)
 
     assert board.check_terminal_position() == ans
